429c6b4bb42e6746c935bb075f68052a56c4b880
Antxon <agoca80@gmail.com>
2019-04-19 19:39:26 +0200

    separate server from main func

---
+++ server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net"
+	"os"
+
+	"github.com/agoca80/tc1/memory"
+	"github.com/agoca80/tc1/server"
+)
+
+// Server ...
+func Server() {
+	memory, err := memory.NewMemory(dump)
+	if err != nil {
+		panic(err)
+	}
+
+	output, err := os.OpenFile(out, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	input, err := os.OpenFile(in, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	listener, err := net.Listen("tcp", ":4000")
+	if err != nil {
+		panic(err)
+	}
+
+	service := server.New(listener, input, output, memory)
+	service.Start()
+
+	err = service.Store(dump)
+	if err != nil {
+		panic(err)
+	}
+}
